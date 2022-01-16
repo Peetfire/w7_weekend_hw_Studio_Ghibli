c488f2be9633967145be1b311dabb20fbff3685c
petermordaunt <peetfire@hotmail.com>
2022-01-16 16:39:49 +0000

    style - css noodling with secondry info. Added transition and minor styling. content - added secondaary content for films.

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,13 +3,13 @@ import ghost from '../img/ghost.png'
 
 const Item = ({data}) => {
 
-    const [style, setStyle] = useState({display: 'none'})
+    const [style, setStyle] = useState({height: '0px'})
 
         const handleMouseIn = () => {
-            setStyle({display: 'block', height: '200px'});
+            setStyle({height: '200px'});
         }
         const handleMouseOut = e => {
-            setStyle({display: 'none'})
+            setStyle({height: '0px'})
         }
 
     return (
@@ -17,10 +17,25 @@ const Item = ({data}) => {
             <img src={data['movie_banner']} />
             <div className="item-info">
                 <div className="primary-info">
-                    <h4>{data.title}</h4>
+                    <h4><b>{data.title}</b></h4>
+                    <h4>{data.original_title}</h4>
                 </div>
                 <div className="secondary-info" style={style}>
-                    <h4 >test</h4>
+                    <div id="desc-wrapper">
+                        <h4><b>Description:</b> {data.description}</h4>
+                    </div>
+                    <div id="line-wrapper">
+                        <h4><b>Director:</b> {data.director} </h4>
+                        <h4><b>Released:</b> {data.release_date}</h4>
+                    </div>
+                    <div id="line-wrapper">
+                        <h4><b>Producer:</b> {data.producer} </h4>
+                        <h4><b>Runtime:</b> {data.running_time} min</h4>
+                    </div>
+                    <div id="line-wrapper">
+                        <h4></h4>
+                        <h4><b>Rotten Tomatoes Score:</b> {data.rt_score}% </h4>
+                    </div>
                 </div>
             </div>
         </div>
